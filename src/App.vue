252721da0e0e477bbc795b8cd67a26d99b1aa68e
or-abdillh <hans.abdillh05@gmail.com>
2024-08-04 09:27:11 +0800

    Init project

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1 class="text-center text-primary">hello</h1>
+    <PencilIcon class="w-12" />
+</template>
+
+<script setup lang="ts">
+import { PencilIcon } from '@heroicons/vue/16/solid'
+</script>
